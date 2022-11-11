48bae54289c1a8fef8c859e4a7f02d5bb13a0e28
CoderGirl <angi.adema@gmail.com>
2022-11-11 13:08:49 -0700

    updated model file index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,26 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE',  //How do I know when to have an onDelete clause and when not to?
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',  //This one may not have the onDelete clause since it would be rare to eliminate a category?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag,
+  foreignKey: 'productTag_id',
+  //Do we need an onDelete or other option?
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag,
+  foreignKey: 'productTag_id',
+});
 
 module.exports = {
   Product,
